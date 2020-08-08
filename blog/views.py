38e481e0579d0e92570f97a7648b6d21afc2ec65
hem67 <39092558+hem67@users.noreply.github.com>
2020-08-08 13:19:50 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView)
+from .models import Post,Comment
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+class PostDetailView
